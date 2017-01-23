941ec0cbf4165ce5d0c6882c79ae6440c329a2af
FreshMan <qinbocai@sina.cn>
2017-01-24 00:29:55 +0800

    Add online user list stock into redis.

---
+++ src/Pc.Information.Model/User/OnlineUserModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Pc.Information.Model.User
+{
+    /// <summary>
+    /// Online user model
+    /// </summary>
+    public class OnlineUserModel
+    {
+        /// <summary>
+        /// User id
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// User name
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Rule type:1:usually user;2:teacher user.
+        /// </summary>
+        public int RuleType { get; set; }
+    }
+}
